270d62189eb86753eac63c320d2549a2cdb18cc7
Shio3001 <taku2000maru@icloud.com>
2021-12-28 03:57:25 +0900

    複数要素

--- MaincraftDistanceCsharp/Form1.Designer.cs
+++ MaincraftDistanceCsharp/Form1.Designer.cs
@@ -28,14 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.text_x1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.text_x2 = new System.Windows.Forms.TextBox();
+            this.text_x = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.text_z2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.text_z1 = new System.Windows.Forms.TextBox();
+            this.text_z = new System.Windows.Forms.TextBox();
             this.Crun = new System.Windows.Forms.Button();
             this.movement_distance = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -43,77 +39,49 @@
             this.straight_distance = new System.Windows.Forms.TextBox();
             this.movement_copy = new System.Windows.Forms.Button();
             this.straight_copy = new System.Windows.Forms.Button();
+            this.position_new = new System.Windows.Forms.Button();
+            this.position_delete = new System.Windows.Forms.Button();
+            this.PositionDataViewList = new System.Windows.Forms.ListView();
+            this.position_change = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // text_x1
-            // 
-            this.text_x1.Location = new System.Drawing.Point(44, 24);
-            this.text_x1.Name = "text_x1";
-            this.text_x1.Size = new System.Drawing.Size(100, 19);
-            this.text_x1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "x1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 56);
+            this.label2.Location = new System.Drawing.Point(467, 25);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 3;
-            this.label2.Text = "x2";
+            this.label2.Text = "x座標";
             // 
-            // text_x2
+            // text_x
             // 
-            this.text_x2.Location = new System.Drawing.Point(44, 49);
-            this.text_x2.Name = "text_x2";
-            this.text_x2.Size = new System.Drawing.Size(100, 19);
-            this.text_x2.TabIndex = 2;
+            this.text_x.Location = new System.Drawing.Point(508, 18);
+            this.text_x.Name = "text_x";
+            this.text_x.Size = new System.Drawing.Size(100, 19);
+            this.text_x.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(161, 56);
+            this.label3.Location = new System.Drawing.Point(614, 25);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(16, 12);
+            this.label3.Size = new System.Drawing.Size(34, 12);
             this.label3.TabIndex = 7;
-            this.label3.Text = "z2";
+            this.label3.Text = "z座標";
             // 
-            // text_z2
+            // text_z
             // 
-            this.text_z2.Location = new System.Drawing.Point(184, 49);
-            this.text_z2.Name = "text_z2";
-            this.text_z2.Size = new System.Drawing.Size(100, 19);
-            this.text_z2.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(161, 31);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(16, 12);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "z1";
-            // 
-            // text_z1
-            // 
-            this.text_z1.Location = new System.Drawing.Point(184, 24);
-            this.text_z1.Name = "text_z1";
-            this.text_z1.Size = new System.Drawing.Size(100, 19);
-            this.text_z1.TabIndex = 4;
+            this.text_z.Location = new System.Drawing.Point(654, 18);
+            this.text_z.Name = "text_z";
+            this.text_z.Size = new System.Drawing.Size(100, 19);
+            this.text_z.TabIndex = 6;
             // 
             // Crun
             // 
-            this.Crun.Location = new System.Drawing.Point(44, 74);
+            this.Crun.Location = new System.Drawing.Point(469, 81);
             this.Crun.Name = "Crun";
-            this.Crun.Size = new System.Drawing.Size(75, 23);
+            this.Crun.Size = new System.Drawing.Size(343, 23);
             this.Crun.TabIndex = 8;
             this.Crun.Text = "計算する";
             this.Crun.UseVisualStyleBackColor = true;
@@ -121,7 +89,7 @@
             // 
             // movement_distance
             // 
-            this.movement_distance.Location = new System.Drawing.Point(23, 136);
+            this.movement_distance.Location = new System.Drawing.Point(469, 132);
             this.movement_distance.Name = "movement_distance";
             this.movement_distance.Size = new System.Drawing.Size(261, 19);
             this.movement_distance.TabIndex = 9;
@@ -129,7 +97,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 121);
+            this.label5.Location = new System.Drawing.Point(467, 117);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(150, 12);
             this.label5.TabIndex = 10;
@@ -138,7 +106,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(21, 166);
+            this.label6.Location = new System.Drawing.Point(467, 162);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(131, 12);
             this.label6.TabIndex = 12;
@@ -146,14 +114,14 @@
             // 
             // straight_distance
             // 
-            this.straight_distance.Location = new System.Drawing.Point(23, 181);
+            this.straight_distance.Location = new System.Drawing.Point(469, 177);
             this.straight_distance.Name = "straight_distance";
             this.straight_distance.Size = new System.Drawing.Size(261, 19);
             this.straight_distance.TabIndex = 11;
             // 
             // movement_copy
             // 
-            this.movement_copy.Location = new System.Drawing.Point(291, 134);
+            this.movement_copy.Location = new System.Drawing.Point(737, 130);
             this.movement_copy.Name = "movement_copy";
             this.movement_copy.Size = new System.Drawing.Size(75, 23);
             this.movement_copy.TabIndex = 13;
@@ -163,7 +131,7 @@
             // 
             // straight_copy
             // 
-            this.straight_copy.Location = new System.Drawing.Point(291, 179);
+            this.straight_copy.Location = new System.Drawing.Point(737, 175);
             this.straight_copy.Name = "straight_copy";
             this.straight_copy.Size = new System.Drawing.Size(75, 23);
             this.straight_copy.TabIndex = 14;
@@ -171,11 +139,55 @@
             this.straight_copy.UseVisualStyleBackColor = true;
             this.straight_copy.Click += new System.EventHandler(this.straight_copy_Click);
             // 
+            // position_new
+            // 
+            this.position_new.Location = new System.Drawing.Point(469, 43);
+            this.position_new.Name = "position_new";
+            this.position_new.Size = new System.Drawing.Size(179, 23);
+            this.position_new.TabIndex = 16;
+            this.position_new.Text = "新規";
+            this.position_new.UseVisualStyleBackColor = true;
+            this.position_new.Click += new System.EventHandler(this.position_new_Click);
+            // 
+            // position_delete
+            // 
+            this.position_delete.Location = new System.Drawing.Point(737, 43);
+            this.position_delete.Name = "position_delete";
+            this.position_delete.Size = new System.Drawing.Size(75, 23);
+            this.position_delete.TabIndex = 17;
+            this.position_delete.Text = "削除";
+            this.position_delete.UseVisualStyleBackColor = true;
+            this.position_delete.Click += new System.EventHandler(this.position_delete_Click);
+            // 
+            // PositionDataViewList
+            // 
+            this.PositionDataViewList.HideSelection = false;
+            this.PositionDataViewList.Location = new System.Drawing.Point(12, 18);
+            this.PositionDataViewList.Name = "PositionDataViewList";
+            this.PositionDataViewList.Size = new System.Drawing.Size(432, 178);
+            this.PositionDataViewList.TabIndex = 18;
+            this.PositionDataViewList.UseCompatibleStateImageBehavior = false;
+            this.PositionDataViewList.SelectedIndexChanged += new System.EventHandler(this.PositionDataViewList_SelectedIndexChanged);
+            // 
+            // position_change
+            // 
+            this.position_change.Location = new System.Drawing.Point(656, 43);
+            this.position_change.Name = "position_change";
+            this.position_change.Size = new System.Drawing.Size(75, 23);
+            this.position_change.TabIndex = 19;
+            this.position_change.Text = "変更";
+            this.position_change.UseVisualStyleBackColor = true;
+            this.position_change.Click += new System.EventHandler(this.position_change_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 222);
+            this.ClientSize = new System.Drawing.Size(832, 209);
+            this.Controls.Add(this.position_change);
+            this.Controls.Add(this.PositionDataViewList);
+            this.Controls.Add(this.position_delete);
+            this.Controls.Add(this.position_new);
             this.Controls.Add(this.straight_copy);
             this.Controls.Add(this.movement_copy);
             this.Controls.Add(this.label6);
@@ -184,13 +196,9 @@
             this.Controls.Add(this.movement_distance);
             this.Controls.Add(this.Crun);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.text_z2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.text_z1);
+            this.Controls.Add(this.text_z);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.text_x2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.text_x1);
+            this.Controls.Add(this.text_x);
             this.Name = "MainForm";
             this.Text = "MaincraftDistance";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -200,15 +208,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox text_x1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox text_x2;
+        private System.Windows.Forms.TextBox text_x;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox text_z2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox text_z1;
+        private System.Windows.Forms.TextBox text_z;
         private System.Windows.Forms.Button Crun;
         private System.Windows.Forms.TextBox movement_distance;
         private System.Windows.Forms.Label label5;
@@ -216,6 +219,10 @@
         private System.Windows.Forms.TextBox straight_distance;
         private System.Windows.Forms.Button movement_copy;
         private System.Windows.Forms.Button straight_copy;
+        private System.Windows.Forms.Button position_new;
+        private System.Windows.Forms.Button position_delete;
+        private System.Windows.Forms.ListView PositionDataViewList;
+        private System.Windows.Forms.Button position_change;
     }
 }
 
--- MaincraftDistanceCsharp/Form1.cs
+++ MaincraftDistanceCsharp/Form1.cs
@@ -6,16 +6,27 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+
+
 using System.Windows.Forms;
 
 namespace MaincraftDistanceCsharp
 {
     public partial class MainForm : Form
+
     {
+        int select = 0;
+
         public MainForm()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            PositionDataViewList.View = View.Details;
+            PositionDataViewList.Columns.Add("座標管理番号");
+            PositionDataViewList.Columns.Add("X座標");
+            PositionDataViewList.Columns.Add("Z座標");
+            PositionDataViewList.MultiSelect = false;
 
         }
 
@@ -26,43 +37,95 @@ namespace MaincraftDistanceCsharp
 
         private void Crun_Click(object sender, EventArgs e)
         {
-            string text_x1_str =text_x1.Text;
-            string text_z1_str = text_z1.Text;
-            string text_x2_str = text_x2.Text;
-            string text_z2_str = text_z2.Text;
 
-            int text_x1_int;
-            int text_z1_int;
-            int text_x2_int;
-            int text_z2_int;
 
-            bool text_x1_bool = int.TryParse(text_x1_str, out text_x1_int);
-            bool text_z1_bool = int.TryParse(text_z1_str, out text_z1_int);
-            bool text_x2_bool = int.TryParse(text_x2_str, out text_x2_int);
-            bool text_z2_bool = int.TryParse(text_z2_str, out text_z2_int);
+            int pos_index_count = PositionDataViewList.Items.Count;
 
-            bool convert_bool = text_x1_bool && text_z1_bool && text_x2_bool && text_z2_bool;
+            Debug.Print("開始");
+            Debug.Print(pos_index_count.ToString());
 
-            if (!convert_bool){
-                return;
+            int accumulation_movement_distance_calculation = 0;
+            int accumulation_pythagorean_proposition_int = 0;
+
+            for (int i = 0; i < pos_index_count - 1; i++)
+            {
+                string pos_x_strB = PositionDataViewList.Items[i].SubItems[1].Text;
+                string pos_z_strB = PositionDataViewList.Items[i].SubItems[2].Text;
+
+                string pos_x_strN = PositionDataViewList.Items[i + 1].SubItems[1].Text;
+                string pos_z_strN = PositionDataViewList.Items[i + 1].SubItems[2].Text;
+
+                Debug.Print("   確認");
+                Debug.Print(pos_x_strB.ToString());
+                Debug.Print(pos_z_strB.ToString());
+                Debug.Print(pos_x_strN.ToString());
+                Debug.Print(pos_z_strN.ToString());
+
+                int pos_x_intB = Convert.ToInt32(pos_x_strB);
+                int pos_z_intB = Convert.ToInt32(pos_z_strB);
+                int pos_x_intN = Convert.ToInt32(pos_x_strN);
+                int pos_z_intN = Convert.ToInt32(pos_z_strN);
+
+
+                int x_distance = Math.Abs(pos_x_intN - pos_x_intB);
+                int y_distance = Math.Abs(pos_z_intN - pos_z_intB);
+
+                int movement_distance_calculation = x_distance + y_distance;
+
+                double pythagorean_proposition_double = Math.Sqrt(Math.Pow(x_distance, 2) + Math.Pow(y_distance, 2));
+                int pythagorean_proposition_int = Convert.ToInt32(pythagorean_proposition_double);
+
+
+                Debug.Print("   出力");
+                Debug.Print(movement_distance_calculation.ToString());
+                Debug.Print(pythagorean_proposition_int.ToString());
+                accumulation_movement_distance_calculation += movement_distance_calculation;
+                accumulation_pythagorean_proposition_int += pythagorean_proposition_int;
+
+                Debug.Print(i.ToString());
             }
 
-            text_x1_int = Convert.ToInt32(text_x1_str);
-            text_z1_int = Convert.ToInt32(text_z1_str);
-            text_x2_int = Convert.ToInt32(text_x2_str);
-            text_z2_int = Convert.ToInt32(text_z2_str);
 
-            int x_distance = Math.Abs(text_x2_int - text_x1_int);
-            int y_distance = Math.Abs(text_z2_int - text_z1_int);
+            movement_distance.Text = accumulation_movement_distance_calculation.ToString();
+            straight_distance.Text = accumulation_pythagorean_proposition_int.ToString();
 
-            int movement_distance_calculation = x_distance + y_distance;
+            //    string text_x1_str =text_x1.Text;
+            //    string text_z1_str = text_z1.Text;
+            //    string text_x2_str = text_x.Text;
+            //    string text_z2_str = text_z.Text;
 
-            double pythagorean_proposition_double = Math.Sqrt(Math.Pow(x_distance,2) + Math.Pow(y_distance, 2));
+            //    int text_x1_int;
+            //    int text_z1_int;
+            //    int text_x2_int;
+            //    int text_z2_int;
 
-            int pythagorean_proposition_int = Convert.ToInt32(pythagorean_proposition_double);
+            //    bool text_x1_bool = int.TryParse(text_x1_str, out text_x1_int);
+            //    bool text_z1_bool = int.TryParse(text_z1_str, out text_z1_int);
+            //    bool text_x2_bool = int.TryParse(text_x2_str, out text_x2_int);
+            //    bool text_z2_bool = int.TryParse(text_z2_str, out text_z2_int);
 
-            movement_distance.Text = movement_distance_calculation.ToString();
-            straight_distance.Text = pythagorean_proposition_int.ToString();
+            //    bool convert_bool = text_x1_bool && text_z1_bool && text_x2_bool && text_z2_bool;
+
+            //    if (!convert_bool){
+            //        return;
+            //    }
+
+            //    text_x1_int = Convert.ToInt32(text_x1_str);
+            //    text_z1_int = Convert.ToInt32(text_z1_str);
+            //    text_x2_int = Convert.ToInt32(text_x2_str);
+            //    text_z2_int = Convert.ToInt32(text_z2_str);
+
+            //    int x_distance = Math.Abs(text_x2_int - text_x1_int);
+            //    int y_distance = Math.Abs(text_z2_int - text_z1_int);
+
+            //    int movement_distance_calculation = x_distance + y_distance;
+
+            //    double pythagorean_proposition_double = Math.Sqrt(Math.Pow(x_distance,2) + Math.Pow(y_distance, 2));
+
+            //    int pythagorean_proposition_int = Convert.ToInt32(pythagorean_proposition_double);
+
+            //    movement_distance.Text = movement_distance_calculation.ToString();
+            //    straight_distance.Text = pythagorean_proposition_int.ToString();
 
         }
 
@@ -76,6 +139,122 @@ namespace MaincraftDistanceCsharp
         {
             Clipboard.SetText(straight_distance.Text);
 
+        }
+
+        int count = 0;
+
+        private List<string> get_position()
+        {
+
+            string text_x_str = text_x.Text;
+            string text_z_str = text_z.Text;
+
+            int text_x_int;
+            int text_z_int;
+
+            bool text_x_bool = int.TryParse(text_x_str, out text_x_int);
+            bool text_z_bool = int.TryParse(text_z_str, out text_z_int);
+
+            bool convert_bool = text_x_bool && text_z_bool;
+
+            if (!convert_bool)
+            {
+                MessageBox.Show("正しい値を入力してね(´・ω・`)",
+                "(∩´﹏`∩)",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+
+                text_x_str = "0";
+                text_z_str = "0";
+
+            }
+
+
+            text_x_int = Convert.ToInt32(text_x_str);
+            text_z_int = Convert.ToInt32(text_z_str);
+
+            List<string> number = new List<string>();
+
+            number.Add(text_x_str);
+            number.Add(text_z_str);
+
+            return number;
+
+        }
+
+        private void position_new_Click(object sender, EventArgs e)
+        {
+            ListViewItem lvi;
+
+            List<string> pos = get_position();
+
+            lvi = PositionDataViewList.Items.Add(count.ToString());
+            lvi.SubItems.Add(pos[0]);
+            lvi.SubItems.Add(pos[1]);
+
+            count++;
+
+        }
+
+        private bool click_judge_position_listview()
+        {
+            int click_count = PositionDataViewList.SelectedItems.Count;
+
+            if (click_count != 1)
+            {
+                MessageBox.Show("必ずひとつだけ選択してね(´・ω・`)",
+                "(∩´﹏`∩)",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+
+                return false;
+            }
+
+
+            return true;
+        }
+
+        
+
+        private void position_delete_Click(object sender, EventArgs e)
+        {
+            PositionDataViewList.Items.Remove(PositionDataViewList.SelectedItems[0]);
+        }
+
+        private void position_change_Click(object sender, EventArgs e)
+        {
+            if (click_judge_position_listview())
+            {
+                PositionDataViewList.SelectedItems[0].SubItems[1].Text = text_x.Text;
+                PositionDataViewList.SelectedItems[0].SubItems[2].Text = text_z.Text;
+            }
+        }
+
+        private void PositionDataViewList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int id = -1;
+            try
+            {
+                id = PositionDataViewList.SelectedItems[0].Index;
+            }
+            catch
+            {
+
+            }
+
+            if (id < 0) return;
+
+            select = id;
+
+            string select_str = PositionDataViewList.SelectedItems[0].SubItems[0].Text;
+            string pos_x_str = PositionDataViewList.SelectedItems[0].SubItems[1].Text;
+            string pos_z_str = PositionDataViewList.SelectedItems[0].SubItems[2].Text;
+
+            int pos_x = Convert.ToInt32(pos_x_str);
+            int pos_z = Convert.ToInt32(pos_z_str);
+
+            text_x.Text = pos_x_str;
+            text_z.Text = pos_z_str;
         }
     }
 }
